bd81bb35c856762584240a07f6c2a48453ab4f88
shivansh <shivanshakb@gmail.com>
2020-11-03 18:04:18 +0530

    attribute

---
+++ Attribute/AttributeEx/Calculator.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using static AttributeEx.Program;
+
+namespace AttributeEx
+{
+    [Author(Name = "Shivansh Tiwari", Email = "s@gmail.com", CreatedAt = "3-11-2020")]
+    class Calculator
+    {
+        [Subroutine(Description = "Add")]
+        public int Add(List<int> nums)
+        {
+            int sum = 0;
+            foreach (int n in nums)
+            {
+                sum += n;
+            }
+            return sum;
+        }
+        [Subroutine(Description = "Subtract")]
+        public int Subtract(int firstNum,int secondNum)
+        {
+            return firstNum - secondNum;
+        }
+        [Subroutine(Description = "Multiply")]
+        public int Multtiply(List<int> nums)
+        {
+            int result = 1;
+            foreach (int n in nums)
+            {
+                result *= n;
+            }
+            return result;
+        }
+        [Subroutine(Description = "Divide")]
+        public int Divide(int firstNum, int secondNum)
+        {            
+            return firstNum / secondNum;
+        }
+    }
+}
---
+++ Attribute/AttributeEx/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace AttributeEx
+{
+    class Program
+    {
+        [AttributeUsage(AttributeTargets.Class)]
+        public class Author : System.Attribute
+        {
+            public string Name { get; set; }
+            public string Email { get; set; }
+            public string CreatedAt { get; set; }
+        }
+
+        [AttributeUsage(AttributeTargets.Method)]
+        public class Subroutine : System.Attribute
+        {
+            public string Description { get; set; }
+        }
+
+
+        #region Main Region
+        static void Main(string[] args)
+        {
+
+            Calculator calculator = new Calculator();
+            Console.WriteLine("How many numbers do you have for addition and multiplication ::");
+            int n = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter the operands::");
+            List<int> nums = new List<int>();
+            for (int i = 0; i < n; i++)
+            {
+                nums.Add(Convert.ToInt32(Console.ReadLine()));
+            }
+            Console.WriteLine("Enter the nums for subtraction and devision");
+            int first = Convert.ToInt32(Console.ReadLine());
+            int second = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine($"Addition result ::{calculator.Add(nums)}");
+            Console.WriteLine($"Subtraction result ::{calculator.Subtract(first,second)}");
+            Console.WriteLine($"Multiplication result ::{ calculator.Multtiply (nums)}");
+            Console.WriteLine($"Division result ::{calculator.Divide(first,second)}");
+        }
+        #endregion
+    }
+}
